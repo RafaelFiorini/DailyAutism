8e77085e2d5a857116bcb0de16e026c6f17e2323
RafaelFiorini <rafaelhenriquefiorini@gmail.com>
2025-09-01 14:45:13 -0300

    primeiro commit

---
+++ src/main/java/com/clinica/dailyautism/DailyAutismApplication.java
@@ -0,0 +1,13 @@
+package com.clinica.dailyautism;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DailyAutismApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DailyAutismApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/clinica/dailyautism/DailyAutismApplicationTests.java
@@ -0,0 +1,13 @@
+package com.clinica.dailyautism;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DailyAutismApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
